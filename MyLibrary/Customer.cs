e5ccfdc8ceb79204169ef93764891c93594e2cc3
jesus <csx02351@educastur.es>
2024-09-25 11:03:01 +0200

    Finiahed Customer Class

--- MyLibrary/Customer.cs
+++ MyLibrary/Customer.cs
@@ -4,6 +4,14 @@ namespace MyLibrary
 {
     public class Customer
     {
+        //se llaman propiedades
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
 
+        //propiedad que me de nombre completo (uso de lambda)
+        public string FuName => $"{FirstName} {Lastname}";
+        
+            
+        
     }
 }
