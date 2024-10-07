4e5ef1046a9a64722cb98e306b9a4de9272d34aa
jesus <csx02351@educastur.es>
2024-10-07 09:29:28 +0200

    preparado para pruebas o geteven numbers

--- MyLibrary/Operations.cs
+++ MyLibrary/Operations.cs
@@ -9,15 +9,37 @@ namespace MyLibrary
     public class Operations
     {
         public int Add(int number1, int number2)
-        { 
-              return number1 + number2; 
-         }
+        {
+            return number1 + number2;
+        }
 
         public bool IsEven(int number)
-        {  return number % 2 == 0; 
-        
+        { return number % 2 == 0;
+
         }
-           
-            
+        
+
+        public IList<int> GetEvenNumbers (int start, int end)
+        {
+            //List<int> evenNumbers = new List<int>();
+            //for (int i = start; i <= end; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        evenNumbers.Add(i);
+            //    }
+                
+
+            //}
+            //return evenNumbers;
+            //comentar varias lineas control + k + c
+            //esto otra manera
+            return Enumerable.Range(start, end).
+                Where(IsEven)
+                .ToList();
+            //hacer un metodo de prueba para esta clase que devuelve metodos junit. de arrays min 35
+        }
     }
-}
+}       
+    
+
---
+++ MyLibraryTest/CustomerNumitTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    [TestFixture]
+    public class CustomerNumitTest
+    {
+        [Test]
+        public void FullName_ReturnEmpty()
+        {
+
+
+
+            //1. Arrage
+            Customer customer = new Customer
+            {
+                FirstName = "john"
+                LastName = "Doe"
+
+            };
+            //2. Act
+            string result = customer.FuName;
+
+            //3.Assert
+            Assert.That(result, Is.Not.WhiteSpace);
+            Assert.That(result, Does.Contain(" "));
+            Assert.That(result, Does.Contain("John"));
+            Assert.That(result, Does.Contain("Doe"));
+            Assert.That(result, Is.EqualTo("John Doe"));
+            Assert.That(result, Does.Contain("John Doe"));
+
+
+
+
+        }
+
+    }
+}
+
+
+
+
